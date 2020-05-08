df9bacfaa4e0481366a4a5ba26a21797dddb5730
Ilya Yudkovich <iyudkovich@kyru.us>
2020-05-08 17:48:51 -0400

    Updates

---
+++ precache-manifest.421f183d1e27eda374c4ae0989431e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087542e775bb1343faa92e9806e5e5d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "994a8bba81360ce09763",
+    "url": "/static/css/main.d172133a.chunk.css"
+  },
+  {
+    "revision": "6b3774867df2fc998a06",
+    "url": "/static/js/2.496f78b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.496f78b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994a8bba81360ce09763",
+    "url": "/static/js/main.a2d4bfc6.chunk.js"
+  },
+  {
+    "revision": "5269df2b328a7ea31ed6",
+    "url": "/static/js/runtime-main.302b843d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
