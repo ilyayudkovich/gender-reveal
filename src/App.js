276bfd6feb816ac071d7a5aee44e5497502b0d7e
Ilya Yudkovich <iyudkovich@kyru.us>
2020-05-08 12:59:20 -0400

    remove comments

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           CONGRATULATIONS
         </p>
-        {/* <a
-          className="App-link"
-          href="https://media.giphy.com/media/pZWz3alofcJmE/giphy.mp4"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          IT'S A BOY!
-        </a> */}
         <iframe src="https://giphy.com/embed/pZWz3alofcJmE" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/pZWz3alofcJmE"></a></p>
       </header>
     </div>
