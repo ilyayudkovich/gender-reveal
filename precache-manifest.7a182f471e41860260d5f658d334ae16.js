fc99507f53547ae9fe9f6c94d40d1f06d2ee776e
Ilya Yudkovich <iyudkovich@kyru.us>
2020-05-08 15:24:55 -0400

    Updates

---
+++ precache-manifest.7a182f471e41860260d5f658d334ae16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1a6c0a534ddc675383793709584fc2",
+    "url": "/brians-boy/index.html"
+  },
+  {
+    "revision": "3866d588f65e9b2425fa",
+    "url": "/brians-boy/static/css/main.d172133a.chunk.css"
+  },
+  {
+    "revision": "6b3774867df2fc998a06",
+    "url": "/brians-boy/static/js/2.496f78b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brians-boy/static/js/2.496f78b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3866d588f65e9b2425fa",
+    "url": "/brians-boy/static/js/main.b64c0c87.chunk.js"
+  },
+  {
+    "revision": "d4ebda0d46fa09c35b77",
+    "url": "/brians-boy/static/js/runtime-main.0d041ac2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brians-boy/static/media/logo.5d5d9eef.svg"
+  }
+]);
